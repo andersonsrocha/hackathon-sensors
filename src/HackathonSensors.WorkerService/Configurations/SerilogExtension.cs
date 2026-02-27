2674edac8ef51fdb012d2fa65a3484548bb012d4
Anderson <andersonsrocha.dev@gmail.com>
2026-02-27 19:07:33 -0300

    fix(serilog): adicionar logs

---
+++ src/HackathonSensors.WorkerService/Configurations/SerilogExtension.cs
@@ -0,0 +1,22 @@
+﻿using NewRelic.LogEnrichers.Serilog;
+using Serilog;
+using Serilog.Events;
+
+namespace HackathonSensors.WorkerService.Configurations;
+
+public static class SerilogExtension
+{
+    public static void AddSerilog(this HostApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .Enrich.WithNewRelicLogsInContext()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {CorrelationId}] {Message}{NewLine}{Exception}")
+            .CreateLogger();
+        
+        builder.Logging.ClearProviders();
+        builder.Logging.AddSerilog(Log.Logger);
+    }
+}
--- src/HackathonSensors.WorkerService/Program.cs
+++ src/HackathonSensors.WorkerService/Program.cs
@@ -1,12 +1,17 @@
 using HackathonSensors.Application.RabbitMQ.Handlers;
 using HackathonSensors.Application.RabbitMQ.Interfaces;
 using HackathonSensors.WorkerService;
+using HackathonSensors.WorkerService.Configurations;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 #region [RabbitMQ]
 builder.Services.AddSingleton<IRabbitMqService, RabbitMqService>();
+#endregion
+
+#region [Serilog]
+builder.AddSerilog();
 #endregion
 
 var host = builder.Build();
