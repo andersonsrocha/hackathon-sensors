353e5af0bf53f5813bd2d267a3ccf4e39858f113
Anderson <andersonsrocha.dev@gmail.com>
2026-02-27 14:22:37 -0300

    feat(v3): gerando a versão 3

--- src/HackathonSensors.WorkerService/Worker.cs
+++ src/HackathonSensors.WorkerService/Worker.cs
@@ -5,26 +5,26 @@ namespace HackathonSensors.WorkerService;
 
 public class Worker(IRabbitMqService service, IConfiguration configuration, ILogger<Worker> logger) : BackgroundService
 {
+    private readonly Guid _plotId = Guid.Parse(configuration["PlotId"] ?? string.Empty);
+    private readonly int _delayInSeconds = int.Parse(configuration["DelayInSeconds"] ?? "10");
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (Guid.TryParse(configuration["PlotId"], out var plotId))
+            logger.LogInformation("Reading data for plot {PlotId}", _plotId);
+            var reading = new ReadingRequest
             {
-                logger.LogInformation("Reading data for plot {PlotId}", plotId);
-                var reading = new ReadingRequest
-                {
-                    PlotId = plotId,
-                    Date = DateTime.Now,
-                    SoilMoisture = Random.Shared.NextDouble() * 100,
-                    Temperature = 15 + Random.Shared.NextDouble() * 20,
-                    Precipitation = Random.Shared.NextDouble() * 10
-                };
-                
-                await service.SendPurchaseAsync(reading);
-            }
+                PlotId = _plotId,
+                Date = DateTime.Now,
+                SoilMoisture = Random.Shared.NextDouble() * 100,
+                Temperature = 15 + Random.Shared.NextDouble() * 20,
+                Precipitation = Random.Shared.NextDouble() * 10
+            };
 
-            await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+            await service.SendPurchaseAsync(reading);
+
+            await Task.Delay(TimeSpan.FromSeconds(_delayInSeconds), stoppingToken);
         }
     }
 }
